468516aee9097f9e1d40e9b6bf63c186c9da53d6
tilaa21 <tilamardha@gmail.com>
2024-06-10 00:15:45 +0700

    Initial commit

---
+++ lib/lat_8_juni/screen_page/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_4/lat_8_juni/screen_page/page_provinsi.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => PageBeranda()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFED8DF),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('gambar/rs4.png', width: 260),
+            SizedBox(height: 15),
+            Text(
+              'MediSearch',
+              style: TextStyle(
+                color: Color(0xFFff7074),
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Poppins',
+              ),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
